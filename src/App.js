4904be1afd7bfe38c80cf21801142fbad8ca7a1f
awalker <revengeofthepencil@gmail.com>
2018-06-07 16:33:02 -0700

    initial commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import WordCounter from './wordcounter';
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <p className="App-intro">
+         Here is the word counter stuff:
+            <WordCounter text="Count the words in here." targetWordCount={10} />
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
