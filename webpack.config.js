93f4658f0ac2ffc9c869840ff277d6b3660355aa
awalker <revengeofthepencil@gmail.com>
2018-06-08 09:20:28 -0700

    updates for babel

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path');
+
+module.exports = {
+ entry : { app : './src/index.js'  },
+    output : {
+     path : path.resolve(__dirname),
+        filename: '[name]-bundle.js'
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                include: path.resolve(__dirname, 'src'),
+                use: ['babel-loader']
+            },
+            {
+                test:/\.css$/,
+                include: path.resolve(__dirname, 'src'),
+                use:['style-loader','css-loader']
+            },
+            {
+                test: /\.svg$/,
+                loader: 'svg-loader'
+            }
+        ]
+    }
+};
